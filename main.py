67344458a35248c77e87789d4d4f200977b17462
bdbrink <brendan.brink@outlook.com>
2023-05-11 18:42:03 -0500

    cleanup formatting

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
 shorten_urls = {}
 
+
 def generate_short_url(length=6):
     chars = string.ascii_letters + string.digits
     short_url = "".join(random.choice(chars) for _ in range(length))
     return short_url
+
 
 @app.route("/", methods=["GET", "POST"]):
 def index():
@@ -23,6 +25,7 @@ def index():
         return f"Shortened URL: {request.url_root}{short_url}"
     return render_template("index.html")
 
+
 @app.route("/<short_url>")
 def redirect_url():
     long_url = shortened_urls.get(short_url)
@@ -30,6 +33,7 @@ def redirect_url():
         return redirect(long_url)
     else:
         return "URL not found", 404
+
 
 if __name__ == "__main__":
     app.run(debug=True)
